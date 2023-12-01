8f6d3c50975f84a456856cebadfa55c2c6a84b19
pjw222 <wpzktm@naver.com>
2023-12-01 11:21:33 +0900

    -commit

---
+++ src/test/java/com/kyungil/webcar/WebcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungil.webcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
