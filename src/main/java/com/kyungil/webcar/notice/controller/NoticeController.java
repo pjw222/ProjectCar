27105f5a282ad045f4849acf9f0c8ea6862429ed
pjw222 <wpzktm@naver.com>
2023-12-16 17:13:20 +0900

    - commit

--- src/main/java/com/kyungil/webcar/notice/controller/NoticeController.java
+++ src/main/java/com/kyungil/webcar/notice/controller/NoticeController.java
@@ -40,36 +40,7 @@ public class NoticeController {
 		return "basic/layout";
 	}
 
-	@GetMapping("/admin/notice")
-	public String getAdminPage(Model model, @RequestParam(defaultValue = "1", name = "page") int page,
-			@RequestParam(defaultValue = "5", name = "pageSize") int pageSize) {
-		List<Notice> pagedNotices = noticeService.getNoticesByPage(page, pageSize);
 
-		int totalBoards = noticeService.getPageCount();
-		int totalPages = (int) Math.ceil((double) totalBoards / pageSize);
-		model.addAttribute("notices", pagedNotices);
-		model.addAttribute("currentPage", page);
-		model.addAttribute("totalPages", totalPages);
-		model.addAttribute("title", "관리자페이지");
-		model.addAttribute("path", "/admin/notice");
-		model.addAttribute("content", "noticeFragment");
-		model.addAttribute("contentHead", "noticeFragmentHead");
-		return "admin/adminlayout";
-	}
-
-	@PostMapping("/admin/notice/add")
-	public String boardAdd(@RequestParam Map<String, String> data, HttpSession session) {
-		try {
-			int userId = (Integer) session.getAttribute("userId");
-
-			noticeService.add(new Notice(data.get("title"), data.get("content"), userId));
-
-			return "redirect:/admin/notice";
-
-		} catch (Exception e) {
-			return "redirect:/";
-		}
-	}
 
 	@GetMapping("/notice/view/{noticeId}")
 	public String showBoardDetail(@PathVariable("noticeId") int noticeId, Model model) {
